5404f12fcea20d9486ab833f52cfdd3d048ed48f
Jason Covington <jason.covington@volusion.com>
2019-02-14 11:19:35 -0600

    Add initial LinkButton (#8)
    * Add initial LinkButton
    
    * Update with latest from master

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import * as Button from './Button';
 import * as Input from './Input';
+import * as LinkButton from './LinkButton';
 
-export { Button, Input };
+export { Button, Input, LinkButton };
