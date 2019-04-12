f09748dcc85712101e2079ee782a93b1bf987933
Jason Covington <jason_covington@volusion.com>
2019-04-12 16:25:26 -0500

    Button updates

--- src/Button/index.js
+++ src/Button/index.js
@@ -1,19 +1,38 @@
+import PropTypes from 'prop-types';
 import styles from './styles';
 
 const name = 'volComponentButton';
 
 const defaultConfig = {
-    buttonStyles: {
+    buttonType: 'button',
+    buttonRole: 'primary',
+    extendClasses: '',
+    primaryButtonStyles: {
         textColor: '#fff',
-        textTransform: 'uppercase',
-        backgroundColor: '#333',
-        borderColor: '#333',
         hoverTextColor: '#fff',
-        hoverBackgroundColor: '#444',
-        hoverBorderColor: '#444',
-        borderThickness: 'basic',
-        size: 'large',
-        rounded: 'none',
+        backgroundColor: '#333',
+        hoverBackgroundColor: '#555',
+        borderColor: '#333333',
+        hoverBorderColor: '#555',
+        fontWeight: '700',
+        textTransform: 'none', // none, uppercase
+        borderThickness: 'basic', // basic, thin, thick, none
+        size: 'medium', // small, medium, large, block
+        rounded: 'medium', // none, small, medium, large, pill
+        growOnHover: false
+    },
+    secondaryButtonStyles: {
+        textColor: '#666',
+        hoverTextColor: '#333',
+        backgroundColor: '#fff',
+        hoverBackgroundColor: '#f2f2f2',
+        borderColor: '#666',
+        hoverBorderColor: '#333',
+        fontWeight: '700',
+        textTransform: 'none', // none, uppercase
+        borderThickness: 'basic', // basic, thin, thick, none
+        size: 'medium', // small, medium, large, block
+        rounded: 'medium', // none, small, medium, large, pill
         growOnHover: false
     }
 };
@@ -21,28 +40,78 @@ const defaultConfig = {
 const factory = (
     { React, ElementPropTypes },
     { StyleSheet, css },
-    globalStyles
+    { globalComponents }
 ) => {
+    const buttonGlobalStyles = globalComponents.volComponentButton;
     const mergedComponentStyles = {
-        ...defaultConfig.buttonStyles,
-        ...(globalStyles.globalComponents['volComponentButton'] || {})
-            .buttonStyles
+        primary: {
+            ...defaultConfig.primaryButtonStyles,
+            ...(buttonGlobalStyles || {}).primaryButtonStyles
+        },
+        secondary: {
+            ...defaultConfig.secondaryButtonStyles,
+            ...(buttonGlobalStyles || {}).secondaryButtonStyles
+        }
     };
-    const classes = StyleSheet.create(
-        styles(name, globalStyles, mergedComponentStyles)
-    );
-
+    const classes = StyleSheet.create(styles(mergedComponentStyles));
     const configSpec = {
+        buttonType: PropTypes.string,
+        buttonRole: PropTypes.string,
+        extendClasses: PropTypes.string,
         children: ElementPropTypes.array,
-        buttonStyles: ElementPropTypes.shape({
+        primaryButtonStyles: ElementPropTypes.shape({
             textColor: ElementPropTypes.color.isRequired,
+            hoverTextColor: ElementPropTypes.color.isRequired,
+            backgroundColor: ElementPropTypes.color.isRequired,
+            hoverBackgroundColor: ElementPropTypes.color.isRequired,
+            borderColor: ElementPropTypes.color.isRequired,
+            hoverBorderColor: ElementPropTypes.color.isRequired,
+            fontWeight: ElementPropTypes.oneOf([
+                '300',
+                '400',
+                '500',
+                '600',
+                '700',
+                '800',
+                '900'
+            ]).isRequired,
             textTransform: ElementPropTypes.oneOf(['uppercase', 'none'])
                 .isRequired,
+            borderThickness: ElementPropTypes.oneOf([
+                'basic',
+                'thin',
+                'thick',
+                'none'
+            ]).isRequired,
+            size: ElementPropTypes.oneOf(['small', 'medium', 'large', 'block'])
+                .isRequired,
+            rounded: ElementPropTypes.oneOf([
+                'none',
+                'small',
+                'medium',
+                'large',
+                'pill'
+            ]).isRequired,
+            growOnHover: ElementPropTypes.bool.isRequired
+        }),
+        secondaryButtonStyles: ElementPropTypes.shape({
+            textColor: ElementPropTypes.color.isRequired,
+            hoverTextColor: ElementPropTypes.color.isRequired,
             backgroundColor: ElementPropTypes.color.isRequired,
+            hoverBackgroundColor: ElementPropTypes.color.isRequired,
             borderColor: ElementPropTypes.color.isRequired,
-            hoverTextColor: ElementPropTypes.color,
-            hoverBackgroundColor: ElementPropTypes.color,
-            hoverBorderColor: ElementPropTypes.color,
+            hoverBorderColor: ElementPropTypes.color.isRequired,
+            fontWeight: ElementPropTypes.oneOf([
+                '300',
+                '400',
+                '500',
+                '600',
+                '700',
+                '800',
+                '900'
+            ]).isRequired,
+            textTransform: ElementPropTypes.oneOf(['uppercase', 'none'])
+                .isRequired,
             borderThickness: ElementPropTypes.oneOf([
                 'basic',
                 'thin',
@@ -66,6 +135,7 @@ const factory = (
         static propTypes = configSpec;
 
         configClasses = ({
+            fontWeight,
             textTransform,
             borderThickness,
             size,
@@ -92,30 +162,74 @@ const factory = (
                 large: 'br3',
                 pill: 'br-pill'
             };
+            const weight = {
+                300: 'fw3',
+                400: 'fw4',
+                500: 'fw5',
+                600: 'fw6',
+                700: 'fw7',
+                800: 'fw8',
+                900: 'fw9'
+            };
             const text = {
-                none: '',
+                none: 'ttn',
                 uppercase: 'ttu'
             };
             const grow = growOnHover ? 'grow' : '';
 
             /* eslint-disable security/detect-object-injection */
-            return `${base} ${text[textTransform]} ${
+            return `${base} ${text[textTransform]} ${weight[fontWeight]} ${
                 borders[borderThickness]
             } ${sizes[size]} ${corners[rounded]} ${grow}`;
             /* eslint-enable security/detect-object-injection */
         };
 
+        getButtonClasses = (role, styles) => {
+            const baseStyles = `tc link ${this.configClasses(styles)}`;
+            if (role === 'secondary') {
+                return `${baseStyles} ${css(
+                    classes.secondaryButton,
+                    classes.secondaryButtonHover
+                )}`;
+            } else {
+                return `${baseStyles} ${css(
+                    classes.primaryButton,
+                    classes.primaryButtonHover
+                )}`;
+            }
+        };
+
         render() {
-            const { children, ...rest } = this.props;
+            const { children, buttonRole, buttonType, ...rest } = this.props;
+            const styles =
+                buttonRole === 'secondary'
+                    ? mergedComponentStyles.secondary
+                    : mergedComponentStyles.primary;
             return (
-                <button
-                    className={`tc ${this.configClasses(
-                        mergedComponentStyles
-                    )} ${css(classes.button, classes.hover)}`}
-                    {...rest}
-                >
-                    {children}
-                </button>
+                <React.Fragment>
+                    {buttonType === 'button' && (
+                        <button
+                            className={this.getButtonClasses(
+                                buttonRole,
+                                styles
+                            )}
+                            {...rest}
+                        >
+                            {children}
+                        </button>
+                    )}
+                    {buttonType === 'link' && (
+                        <a
+                            className={this.getButtonClasses(
+                                buttonRole,
+                                styles
+                            )}
+                            {...rest}
+                        >
+                            {children}
+                        </a>
+                    )}
+                </React.Fragment>
             );
         }
     };
--- src/Button/styles.js
+++ src/Button/styles.js
@@ -1,32 +1,35 @@
-export default (
-    name,
-    { color, typography },
-    {
-        textColor,
-        backgroundColor,
-        borderColor,
-        hoverTextColor,
-        hoverBackgroundColor,
-        hoverBorderColor
-    }
-) => {
-    const styles = {
-        button: {
-            fontFamily: typography.fontFamily,
-            backgroundColor: backgroundColor || color.primary,
-            borderColor: borderColor || color.primary,
-            color: textColor || 'white'
+export default ({ primary, secondary }) => {
+    console.log('primary', primary); // eslint-disable-line
+    console.log('secondary', secondary); // eslint-disable-line
+
+    return {
+        primaryButton: {
+            backgroundColor: primary.backgroundColor,
+            borderColor: primary.borderColor,
+            color: primary.textColor || 'white'
         },
-        hover: {
+        primaryButtonHover: {
             ':hover': {
-                backgroundColor: hoverBackgroundColor || color.primary,
-                borderColor: hoverBorderColor || color.primary,
-                color: hoverTextColor || 'white',
+                backgroundColor: primary.hoverBackgroundColor,
+                borderColor: primary.hoverBorderColor,
+                color: primary.hoverTextColor || 'white',
+                textDecoration: 'none',
+                transition: 'all 0.2s ease-in-out'
+            }
+        },
+        secondaryButton: {
+            backgroundColor: secondary.backgroundColor,
+            borderColor: secondary.borderColor,
+            color: secondary.textColor || 'white'
+        },
+        secondaryButtonHover: {
+            ':hover': {
+                backgroundColor: secondary.hoverBackgroundColor,
+                borderColor: secondary.hoverBorderColor,
+                color: secondary.hoverTextColor || 'white',
                 textDecoration: 'none',
                 transition: 'all 0.2s ease-in-out'
             }
         }
     };
-
-    return styles;
 };
