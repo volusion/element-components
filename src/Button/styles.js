0e24f7a11a2ee85eecfcd293620da3b893c8a21d
Chris Decker <chris.decker@volusion.com>
2020-01-10 11:55:06 -0600

    fix: Adds missing link pseudo effects css.

--- src/Button/styles.js
+++ src/Button/styles.js
@@ -5,7 +5,15 @@ export default ({ primary, secondary }) => {
             textDecoration: 'none',
             transition: 'color .15s ease-in',
             display: 'inline-block',
-            cursor: 'pointer'
+            cursor: 'pointer',
+            ':hover': { transition: 'color .15s ease-in' },
+            ':active': { transition: 'color .15s ease-in' },
+            ':visited': { transition: 'color .15s ease-in' },
+            ':link': { transition: 'color .15s ease-in' },
+            ':focus': {
+                transition: 'color .15s ease-in',
+                outline: '1px dotted currentColor'
+            }
         },
         borderBasic: {
             borderStyle: 'solid',
