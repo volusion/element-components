42df472816905fff1a5b4e6da1024b0d47a1e66b
Chris Decker <chris.decker@volusion.com>
2020-03-31 10:03:03 -0500

    wip(tests): Removes old jest config files.
    - jest configs shoudl be handled by element-block-scripts.

--- jest.setup.js
+++
@@ -1,9 +0,0 @@
-/* eslint no-console: "off" */
-
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import { StyleSheetTestUtils } from 'aphrodite';
-
-StyleSheetTestUtils.suppressStyleInjection();
-
-configure({ adapter: new Adapter() });
