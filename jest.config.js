e701e34f44c766cb119167552740f6591343c9fe
Cameron Jones <cameron.jones@volusion.com>
2020-03-31 10:28:03 -0500

    chore: update config

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,34 @@
-
 module.exports = {
-    setupFilesAfterEnv: ['<rootDir>/testConfig.ts'],
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-    moduleDirectories: ['node_modules', 'src'],
-    transform: {
-        '^.+\\.(ts|tsx)$': 'ts-jest'
+  setupFilesAfterEnv: [
+    '<rootDir>/testConfig.ts',
+  ],
+  moduleFileExtensions: [
+    'js',
+    'jsx',
+    'ts',
+    'tsx',
+  ],
+  moduleDirectories: [
+    'node_modules',
+    'src',
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  transformIgnorePatterns: [
+    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$',
+  ],
+  snapshotSerializers: [
+    'enzyme-to-json/serializer',
+  ],
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.json',
     },
-    transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$'],
-    snapshotSerializers: ['enzyme-to-json/serializer'],
-    globals: {
-        'ts-jest': {
-            tsConfigFile: 'tsconfig.json'
-        }
-    },
-    rootDir: '.',
-    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
-    // testRegex: 'src/ElementBlock/__test__/Block.spec.ts',
-    verbose: true
-};
+  },
+  rootDir: '.',
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
+  verbose: true,
+  preset: 'ts-jest',
+  testMatch: null,
+}
