66ad6f30b1070cfbf56d93faaaef3d7e51a99551
Cameron Jones <cameron.jones@volusion.com>
2020-03-31 12:22:41 -0500

    fix lint errors

--- jest.config.js
+++ jest.config.js
@@ -1,34 +1,20 @@
 module.exports = {
-  setupFilesAfterEnv: [
-    '<rootDir>/testConfig.ts',
-  ],
-  moduleFileExtensions: [
-    'js',
-    'jsx',
-    'ts',
-    'tsx',
-  ],
-  moduleDirectories: [
-    'node_modules',
-    'src',
-  ],
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
-  transformIgnorePatterns: [
-    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$',
-  ],
-  snapshotSerializers: [
-    'enzyme-to-json/serializer',
-  ],
-  globals: {
-    'ts-jest': {
-      tsConfig: 'tsconfig.json',
+    setupFilesAfterEnv: ['<rootDir>/testConfig.ts'],
+    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+    moduleDirectories: ['node_modules', 'src'],
+    transform: {
+        '^.+\\.(ts|tsx)$': 'ts-jest'
     },
-  },
-  rootDir: '.',
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
-  verbose: true,
-  preset: 'ts-jest',
-  testMatch: null,
-}
+    transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$'],
+    snapshotSerializers: ['enzyme-to-json/serializer'],
+    globals: {
+        'ts-jest': {
+            tsConfig: 'tsconfig.json'
+        }
+    },
+    rootDir: '.',
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
+    verbose: true,
+    preset: 'ts-jest',
+    testMatch: null
+};
