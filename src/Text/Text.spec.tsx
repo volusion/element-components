bb082a7aa10145d4697778230225b20cef3f0c05
Cameron Jones <cameron.jones@volusion.com>
2020-04-02 13:55:00 -0500

    testing

--- src/Text/Text.spec.tsx
+++ src/Text/Text.spec.tsx
@@ -4,20 +4,27 @@ import { block as Text, defaultConfig } from './index';
 import { mockGlobalSettings as globalSettings } from '../__fixtures__/mockGlobalSettings';
 
 describe('The <Text /> Component', () => {
-    /* let props: ImageProps; */
+    const renderWithContent = (content: string) => render(<Text content={content} globalSettings={globalSettings} />)
 
-    /* beforeEach(() => { */
-    /*     props = { */
-    /*         ...defaultConfig, */
-    /*         globalSettings, */
-    /*         utils: { */
-    /*             isAmpRequest: false */
-    /*         } */
-    /*     }; */
-    /* }); */
-
-    it('matches the snapshot', () => {
-        mount(<Text {...defaultConfig} globalSettings={globalSettings} />)
+    describe('with default props', () => {
+        it('matches the snapshot', () => {
+            const rendered = render(<Text {...defaultConfig} globalSettings={globalSettings} />);
+            expect(rendered).toMatchSnapshot();
+        });
     });
+
+    describe('with empty content', () => {
+        it('matches the snapshot', () => {
+            expect(renderWithContent('')).toMatchSnapshot();
+        });
+    })
+
+    describe('with custom content', () => {
+        it('matches the snapshot', () => {
+            expect(renderWithContent('<h1>Test Content</h1>')).toMatchSnapshot();
+            expect(renderWithContent('Test Content')).toMatchSnapshot();
+            expect(renderWithContent('<p>Test Content</p>')).toMatchSnapshot();
+        });
+    })
 });
 
