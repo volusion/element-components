4bef87cc5d9d3c2e572c2d7b5302b9827d0412b1
Chris Decker <chris.decker@volusion.com>
2020-04-07 10:07:17 -0500

    chore: Updates styles with temp values for globals

--- src/ElementBlock/src/getStyles.ts
+++ src/ElementBlock/src/getStyles.ts
@@ -7,38 +7,38 @@ export const getStyles = (
     const mobileStyles =
         blockConfig.showOnMobile === false
             ? {
-                  '@media only screen and (max-width: 30em)': {
-                      display: 'none'
-                  }
-              }
+                '@media only screen and (max-width: 30em)': {
+                    display: 'none'
+                }
+            }
             : {};
     const tabletStyles =
         blockConfig.showOnTablet === false
             ? {
-                  '@media only screen and (min-width: 30em) and (max-width: 60em)': {
-                      display: 'none'
-                  }
-              }
+                '@media only screen and (min-width: 30em) and (max-width: 60em)': {
+                    display: 'none'
+                }
+            }
             : {};
     const desktopStyles =
         blockConfig.showOnDesktop === false
             ? {
-                  '@media only screen and (min-width: 60em)': {
-                      display: 'none'
-                  }
-              }
+                '@media only screen and (min-width: 60em)': {
+                    display: 'none'
+                }
+            }
             : {};
     return {
         wrapper: {
             width: '100%',
             maxWidth: blockConfig.isFullWidth
                 ? undefined
-                : globalSettings.__________,
+                : '96rem',
             paddingRight: blockConfig.hasHorizontalPadding
-                ? globalSettings.__________
+                ? '1rem'
                 : undefined,
             paddingLeft: blockConfig.hasHorizontalPadding
-                ? globalSettings.__________
+                ? '1rem'
                 : undefined,
             paddingTop: getPaddingAmount(
                 blockConfig.topPadding,
@@ -59,7 +59,7 @@ const getPaddingAmount = (
     paddingSize: 'None' | 'Small' | 'Medium' | 'Large',
     globalSettings: ObjectLiteral
 ) => {
-    if (/^small$/i.test(paddingSize)) return globalSettings.__________;
-    if (/^medium$/i.test(paddingSize)) return globalSettings.__________;
-    if (/^large$/i.test(paddingSize)) return globalSettings.__________;
+    if (/^small$/i.test(paddingSize)) return '0.5rem';
+    if (/^medium$/i.test(paddingSize)) return '1rem';
+    if (/^large$/i.test(paddingSize)) return '1.5rem';
 };
