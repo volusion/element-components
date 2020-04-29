59205c3c95c7178eefe683c60ddd35bd92f28a50
Jared Svenson <jared@lolay.com>
2020-04-29 17:13:33 -0600

    Add Logo tests.

---
+++ src/Logo/Logo.spec.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { mount, render } from 'enzyme';
+import { block as Logo, LogoProps } from './index';
+import { defaultConfig } from './config';
+import { block as Text } from '../Text';
+import { block as Image } from '../Image';
+import { mockGlobalSettings as globalSettings } from '../__fixtures__/mockGlobalSettings';
+
+describe('Logo component', () => {
+    let props: LogoProps;
+
+    beforeEach(() => {
+        props = {
+            ...defaultConfig,
+        };
+    });
+
+    it('renders a text logo', () => {
+        const textProps = {
+            globalSettings,
+            htmlString: 'Test Store Name',
+        };
+        const TextComponent = () => <Text {...textProps}  />;
+        props.StoreName = TextComponent;
+
+        const renderedTextLogo = mount(<Logo {...props} />);
+        expect(renderedTextLogo.render()).toMatchSnapshot();
+    });
+
+    it('renders an image logo', () => {
+        const imageProps = {
+            globalSettings,
+            src: 'http://example.com/test.jpg',
+            alt: '',
+            title: '',
+            height: 10,
+            width: 10,
+            utils: {
+                isAmpRequest: false
+            }
+        };
+        const ImageComponent = () => <Image {...imageProps} />;
+        props.style = 'image';
+        props.Image = ImageComponent;
+
+        const renderedImageLogo = mount(<Logo {...props} />);
+        expect(renderedImageLogo.render()).toMatchSnapshot();
+    });
+});
